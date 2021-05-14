02a4d5c72dd0fe37f3203c278c48fae79e57cb5b
Aysha Fatima <ayshafatima346@gmail.com>
2021-05-14 06:04:48 +0500

    Work Completed

---
+++ android/app/src/main/kotlin/com/example/aysha/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aysha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Assignment#1',
+      home: Scaffold(
+        body: Text("Aysha Fatima\nayshafatima346@gmail.com"),
+      )
+    );
+  }
+}
